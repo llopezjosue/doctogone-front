c4b877bc0b800a7bdf9c4618a7c5c362d5532f7c
Aline <alinepascal@gmail.com>
2020-06-25 22:37:48 +0200

    stylisation et adaptation du graphique

--- src/components/Caregiver/HisStats.jsx
+++ src/components/Caregiver/HisStats.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Chart from "./Chart"
+import "./Chart.css";
 
 function HisStats() {
   return <div className="stats">
+    <h2 className="chartsName">My beloved stats</h2>
     <Chart />
   </div>;
 }
