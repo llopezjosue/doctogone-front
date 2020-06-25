432a2c7e92838ed1d3800590f4d1c826d83fc9f0
Louis <colletlouis@gmail.com>
2020-06-25 20:01:05 +0200

    Added fonts

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import './fonts/roboto/Roboto-Regular.ttf';
+import './fonts/centra/Centra No2 Bold.ttf';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
