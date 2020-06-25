177ff96f41a0c4ba956eefaeaae9fba33d9e4481
PaulCrtr <paul.th.cartier@gmail.com>
2020-06-25 18:06:02 +0200

    added accordion on form

--- src/components/Caregiver/PostEvent.jsx
+++ src/components/Caregiver/PostEvent.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
+import { Accordion, Card } from "react-bootstrap";
 
 class PostEvent extends React.Component {
   constructor(props) {
@@ -38,59 +39,72 @@ class PostEvent extends React.Component {
 
   render() {
     return (
-      <Form>
-        <FormGroup>
-          <Label for="title">Title</Label>
-          <Input
-            type="text"
-            name="title"
-            id="title"
-            placeholder="Enter your text"
-            value={this.state.title}
-            onChange={this.handleChangeTitle}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="description">Description</Label>
-          <Input
-            type="text"
-            name="description"
-            id="description"
-            placeholder="Enter your text"
-            value={this.state.description}
-            onChange={this.handleChangeDescription}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="time">Time</Label>
-          <Input
-            type="time"
-            name="time"
-            id="time"
-            placeholder="Enter the hour"
-            value={this.state.hour}
-            onChange={this.handleChangeHour}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleSelect">Select</Label>
-          <Input
-            type="select"
-            name="category"
-            id="category"
-            onChange={this.handleChangeCategory}
-          >
-            <option value="" selected disabled hidden>
-              Choose a category
-            </option>
-            <option value="medic">medic</option>
-            <option value="meal">meal</option>
-            <option value="sleep">sleep</option>
-            <option value="social">social</option>
-          </Input>
-        </FormGroup>
-        <Button onClick={this.handleSubmit}>Submit</Button>
-      </Form>
+      <Accordion defaultActiveKey="1">
+        <Card>
+          <Card.Header>
+            <Accordion.Toggle as={Button} variant="link" eventKey="0">
+              Add a new event
+            </Accordion.Toggle>
+          </Card.Header>
+          <Accordion.Collapse eventKey="0">
+            <Card.Body>
+              <Form className="post-form">
+                <FormGroup>
+                  <Label for="title">Title</Label>
+                  <Input
+                    type="text"
+                    name="title"
+                    id="title"
+                    placeholder="Enter your text"
+                    value={this.state.title}
+                    onChange={this.handleChangeTitle}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="description">Description</Label>
+                  <Input
+                    type="text"
+                    name="description"
+                    id="description"
+                    placeholder="Enter your text"
+                    value={this.state.description}
+                    onChange={this.handleChangeDescription}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="time">Time</Label>
+                  <Input
+                    type="time"
+                    name="time"
+                    id="time"
+                    placeholder="Enter the hour"
+                    value={this.state.hour}
+                    onChange={this.handleChangeHour}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="exampleSelect">Select</Label>
+                  <Input
+                    type="select"
+                    name="category"
+                    id="category"
+                    onChange={this.handleChangeCategory}
+                  >
+                    <option value="" selected disabled hidden>
+                      Choose a category
+                    </option>
+                    <option value="medic">medic</option>
+                    <option value="meal">meal</option>
+                    <option value="sleep">sleep</option>
+                    <option value="social">social</option>
+                  </Input>
+                </FormGroup>
+                <Button onClick={this.handleSubmit}>Submit</Button>
+              </Form>
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      </Accordion>
     );
   }
 }
