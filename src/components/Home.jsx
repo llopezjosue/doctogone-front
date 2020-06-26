49524cac708e5692e81e07fe865f2de8fe5e125e
tzoitzeot <tristan.maciag@icloud.com>
2020-06-26 07:22:05 +0200

    finish home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,9 +10,9 @@ function Home() {
           <img src="/icons/logo-2.svg"></img>
         </div>
         <section className="item">
-          <div className="item-text">
+          <div className="item-text hello">
             <h1>Hello !</h1>
-            <p>Tell us who you are</p>
+            <p>Tell us who you are ?</p>
           </div>
           <Link to="/login" type="button">
             <div className="button light">
@@ -29,7 +29,7 @@ function Home() {
         </section>
 
         <div className="illustration-home">
-          <img src="/img/home.png"></img>
+          <img className="w-100-img " src="/img/home.png"></img>
         </div>
 
         <div></div>
@@ -38,9 +38,7 @@ function Home() {
       <div className="light">
         <div className="container">
           <h1>Why Finding Memo ? </h1>
-          <p>
-          To support our loved ones through Alzheimer's
-          </p>
+          <p className="textSizeLight">To support our loved ones through Alzheimer's</p>
           <div className="button">
             <img src="/icons/fleche-white.svg"></img>
             <p>More information</p>
@@ -49,16 +47,34 @@ function Home() {
       </div>
 
       <div className="img-block">
-        <img src="/img/home-2.png"></img>
+        <img className="w-100-img " src="/img/home-2.png"></img>
       </div>
 
       <div className="data-number">
-        <h1>50 million</h1>
-        <p>The number of Alzheimer's patients in the World</p>
-        <h1>100 million</h1>
-        <p>Projected number of Alzheimer's patients in 2040</p>
-        <h1>$250,174</h1>
-        <p>The total lifetime cost estimation of care for someone with Alzheimer dementia in 2018.</p>
+        <div className="infosStat">
+        <img className=" " src="/icons/a.svg"></img>
+          <h1>50 million</h1>
+          <p>The number of Alzheimer's patients in the World</p>
+        </div>
+
+     
+          <div className="infosStat">
+          <img className=" " src="/icons/b.svg"></img>
+            <h1>100 million</h1>
+            <p>Projected number of Alzheimer's patients in 2040</p>
+          </div>
+      
+
+      
+          <div className="infosStat">
+          <img className=" " src="/icons/c.svg"></img>
+            <h1>$250,174</h1>
+            <p>
+              The total lifetime cost estimation of care for someone with Alzheimer
+              dementia in 2018.
+            </p>
+         
+        </div>
 
         <p className="copyright">Copyright - 2020 Doctogone</p>
       </div>
